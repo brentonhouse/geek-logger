b8630897561ef6d3c64dd3eec6f16209df706f79
Brenton House <brenton.house@gmail.com>
2020-10-08 18:50:33 -0500

    :thumbsup: v1.0.8 :heavy_minus_sign: Bumping version for project

--- config/formats.js
+++ config/formats.js
@@ -154,7 +154,13 @@ exports.events_color = (args = {}) => {
 	const clone = { ...args };
 	const { message = '' } = clone;
 
-	clone.message = red.bold('Event Generated: ') + cyan(message);
+	if (![ undefined, null ].includes(args.event_value)) {
+		clone.message = `${red.bold('Event Generated:')} ${cyan(message)} -- ${green(args.event_value)}`;
+	} else {
+		clone.message = `${red.bold('Event Generated:')} ${cyan(message)}`;
+	}
+
+	// clone.message = red.bold('Event Generated: ') + cyan(message);
 	return exports.timestamp_message_args_color(clone);
 
 };
@@ -162,36 +168,6 @@ exports.events_color = (args = {}) => {
 exports.events_color_simple = (args = {}) => {
 	const clone = { ...args };
 	const { message = '' } = clone;
-	// let extra;
-
-	// switch (message) {
-	// 	case 'screen_view':
-	// 		extra = args.screen_name;
-	// 		break;
-
-	// 	case 'app_first_launch_update':
-	// 		extra = args.app_previous_version;
-	// 		break;
-
-	// 	case 'toggle_beta':
-	// 		extra = args.allow_beta_updates;
-	// 		break;
-
-	// 	case 'toggle_anonymous':
-	// 		extra = args.anonymize_name;
-	// 		break;
-
-	// 	case 'article_open':
-	// 		extra = args.article_id;
-	// 		break;
-
-	// 	case 'office_select':
-	// 		extra = args.new_office_id;
-	// 		break;
-
-	// 	default:
-
-	// }
 
 	if (![ undefined, null ].includes(args.event_value)) {
 		clone.message = `${red.bold('Event Generated:')} ${cyan(message)} -- ${green(args.event_value)}`;
