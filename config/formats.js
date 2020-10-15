c9470c9f51ae09052a734ec738a63793ceb8f256
Brenton House <brenton.house@gmail.com>
2020-10-15 16:02:10 -0500

    update

--- config/formats.js
+++ config/formats.js
@@ -1,6 +1,7 @@
 /* eslint-disable eqeqeq */
 /* eslint-disable no-eq-null */
 const ansiColors = require('ansi-colors');
+const JSONC = require('@geek/jsonc');
 
 // eslint-disable-next-line no-unused-vars
 const { black, red, green, yellow, blue, magenta, cyan, white, gray, bold } = ansiColors;
@@ -120,9 +121,14 @@ exports.timestamp_message = ({ timestamp = new Date().toISOString(), message = '
 	return `${timestamp} - ${message}`;
 };
 
-exports.debug_message_color = ({ namespace, timestamp = new Date().toISOString(), message = '' }) => {
+exports.debug_message_color = ({ namespace, timestamp = new Date().toISOString(), message = '' } = {}) => {
 	const ns_color = getNamespaceColor(namespace);
 	return `${reset}${ns_color(namespace)} ${gray(message)} ${ns_color(timestamp)}`;
+};
+
+exports.debug_message_args_color = ({ namespace, timestamp = new Date().toISOString(), message = '', ...args } = {}) => {
+	const ns_color = getNamespaceColor(namespace);
+	return `${reset}${ns_color(namespace)} ${gray(message)} ${ns_color(timestamp)}\n${JSONC.colorify(args, null, 2)}`;
 };
 
 
@@ -165,7 +171,7 @@ exports.events_color = (args = {}) => {
 
 };
 
-exports.events_color_simple = (args = {}) => {
+exports.events_color_simple2 = (args = {}) => {
 	const clone = { ...args };
 	const { message = '' } = clone;
 
@@ -176,6 +182,18 @@ exports.events_color_simple = (args = {}) => {
 	}
 
 	return exports.timestamp_message_color(clone);
+};
 
+exports.events_color_simple = (args = {}) => {
+	const clone = { ...args };
+	const { message = '' } = clone;
+
+	if (![ undefined, null ].includes(args.event_value)) {
+		clone.message = `${red.bold('Event Generated:')} ${cyan(message)} -- ${green(args.event_value)}`;
+	} else {
+		clone.message = `${red.bold('Event Generated:')} ${cyan(message)}`;
+	}
+
+	return exports.debug_message_color(clone);
 };
 
